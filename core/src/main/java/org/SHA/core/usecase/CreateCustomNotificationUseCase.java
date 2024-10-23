49f885cef47718a68148a2526d5335934685d814
Markus Olsen <markusol@hiof.no>
2024-10-23 13:09:10 +0200

    test skrev hei i usecase createcostumnotification

--- core/src/main/java/org/SHA/core/usecase/CreateCustomNotificationUseCase.java
+++ core/src/main/java/org/SHA/core/usecase/CreateCustomNotificationUseCase.java
@@ -3,7 +3,7 @@ package org.SHA.core.usecase;
 import org.SHA.core.domain.CustomNotification;
 import org.SHA.core.port.NotificationRepository;
 
-//heihei
+//heiheihei
 public class CreateCustomNotificationUseCase {
     private final NotificationRepository notificationRepository;
 
