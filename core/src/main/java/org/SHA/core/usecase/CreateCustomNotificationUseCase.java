7aae22ae37faaf840887777ffe0c7f002a3a3fb9
Hedpedz <143394250+Hedpedz@users.noreply.github.com>
2024-11-19 00:45:26 +0100

    Lagt til kommentarer og feilhåndtering.

--- core/src/main/java/org/SHA/core/usecase/CreateCustomNotificationUseCase.java
+++ core/src/main/java/org/SHA/core/usecase/CreateCustomNotificationUseCase.java
@@ -5,8 +5,12 @@ import java.time.LocalDateTime;
 // Use case klasse for å opprette tilpassede varsler
 public class CreateCustomNotificationUseCase {
 
-    // Oppretter og returnerer et CustomNotification-objekt
+    // Oppretter og returnerer et CustomNotification-objekt og validerer inputdata før opprettelsen av et varsel.
     public CustomNotification create(String id, String message, String recipient, LocalDateTime scheduledTime) {
+        if (id == null || message == null || recipient == null || scheduledTime == null) {
+            throw new IllegalArgumentException("Alle parametere må være satt.");
+        }
         return new CustomNotification(id, message, recipient, scheduledTime);
     }
+
 }
