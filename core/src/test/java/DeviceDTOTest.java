37b40fafd4ad4d1d77a531e1cc5e59d7a0113448
Benjamin-tech02 <yamena@hiof.no>
2024-11-18 16:20:10 +0100

    test
    lage testklasse for DeviceDTO

---
+++ core/src/test/java/DeviceDTOTest.java
@@ -0,0 +1,41 @@
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.SHA.core.dto.DeviceDTO;
+
+public class DeviceDTOTest {
+
+    @Test
+    public void testConstructorAndGetters() {
+
+        DeviceDTO device = new DeviceDTO("12345", "Sensor", "Active");
+
+        assertEquals("12345", device.getDeviceId());
+        assertEquals("Sensor", device.getType());
+        assertEquals("Active", device.getStatus());
+    }
+
+    @Test
+    public void testSetDeviceId() {
+        DeviceDTO device = new DeviceDTO("12345", "Sensor", "Active");
+
+        device.setDeviceId("67890");
+        assertEquals("67890", device.getDeviceId());
+    }
+
+    @Test
+    public void testSetType() {
+        DeviceDTO device = new DeviceDTO("12345", "Sensor", "Active");
+
+        device.setType("Camera");
+        assertEquals("Camera", device.getType());
+    }
+
+    @Test
+    public void testSetStatus() {
+        DeviceDTO device = new DeviceDTO("12345", "Sensor", "Active");
+
+        device.setStatus("Inactive");
+        assertEquals("Inactive", device.getStatus());
+    }
+}
