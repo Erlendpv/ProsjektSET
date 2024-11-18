1b4168b01c6457007d64bcc8a6f01a7074de2b5b
Hedpedz <143394250+Hedpedz@users.noreply.github.com>
2024-11-18 16:05:43 +0100

    Sørget for at klassen håndterer datalagring for alle typer varsler (Premade og Custom), optimalisert databaseforespørsler og fjernet unødvendige metoder.

--- core/src/main/java/org/SHA/core/port/NotificationRepository.java
+++ core/src/main/java/org/SHA/core/port/NotificationRepository.java
@@ -2,8 +2,21 @@ package org.SHA.core.port;
 
 import org.SHA.core.domain.Notification;
 
-public interface NotificationRepository {
-    Notification findById(String notificationId);
-    void save(Notification notification);
-    void delete(String notificationId);
+import java.util.ArrayList;
+import java.util.List;
+
+public class NotificationRepository {
+    private List<Notification> notifications = new ArrayList<>();
+
+    public void save(Notification notification) {
+        notifications.add(notification);
+    }
+
+    public List<Notification> findAll() {
+        return new ArrayList<>(notifications);
+    }
+
+    public void clearAll() {
+        notifications.clear();
+    }
 }
