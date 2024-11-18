9100f703b32f914ee73f7bda0888b70a0fa300a5
AndreasCF202 <andreaci@hiof.no>
2024-11-18 15:51:43 +0100

    Forbedret grensesnittet for hvordan vi skal samhandle med et datalagringssystem

--- core/src/main/java/org/SHA/core/port/UserRepository.java
+++ core/src/main/java/org/SHA/core/port/UserRepository.java
@@ -1,9 +1,12 @@
 package org.SHA.core.port;
 
 import org.SHA.core.domain.User;
+import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository {
-    User findById(String userId);
-    void save(User user);
-    void delete(String userId);
+    Optional<User> findById(String userId); //Finne bruker gjennom userId, Optional hvis bruker ikke eksisterer.
+    List<User> findByUsername(String username); //Nytt mulig søk som kan brukes, for å finne bruker.
+    void save(User user); //Lagrer en bruker i databasen
+    void delete(String userId); //Sletter en bruker i databasen
 }
