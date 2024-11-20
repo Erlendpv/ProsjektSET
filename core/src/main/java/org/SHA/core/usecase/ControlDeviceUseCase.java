27107008a031209b68cae7cf0f5794222dbea685
Benjamin-tech02 <yamena@hiof.no>
2024-11-20 21:26:58 +0100

    addDeleteUseCase
    implementere ferdig en en AddDeleteUserUseCase klasse som funker på samme måte som AddDeleteDeviceUsecase

--- core/src/main/java/org/SHA/core/usecase/ControlDeviceUseCase.java
+++ core/src/main/java/org/SHA/core/usecase/ControlDeviceUseCase.java
@@ -1,15 +1,30 @@
 package org.SHA.core.usecase;
 
 import org.SHA.core.port.DeviceControlService;
+import java.util.HashMap;
+import java.util.Map;
 
-public class ControlDeviceUseCase {
-    private final DeviceControlService deviceControlService;
+public class ControlDeviceUseCase implements DeviceControlService {
+    // En enkel måte å lagre statusen til enheter på
+    private final Map<String, String> deviceStatusMap = new HashMap<>();
 
-    public ControlDeviceUseCase(DeviceControlService deviceControlService){
-        this.deviceControlService = deviceControlService;
+    @Override
+    public void sendCommand(String deviceId, String command) {
+        // Endre statusen basert på kommandoen
+        if ("turn on".equalsIgnoreCase(command)) {
+            deviceStatusMap.put(deviceId, "ACTIVE");
+            System.out.println("Command sent to device " + deviceId + ": turn on");
+        } else if ("turn off".equalsIgnoreCase(command)) {
+            deviceStatusMap.put(deviceId, "INACTIVE");
+            System.out.println("Command sent to device " + deviceId + ": turn off");
+        } else {
+            System.out.println("Unknown command: " + command);
+        }
     }
-    public void execute(String deviceId, String command){
-        deviceControlService.sendCommand(deviceId, command);
+
+    @Override
+    public String getDeviceStatus(String deviceId) {
+        // Returner statusen til enheten, eller "UNKNOWN" hvis den ikke finnes
+        return deviceStatusMap.getOrDefault(deviceId, "UNKNOWN");
     }
 }
-
