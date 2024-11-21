a353c819c7e523ebfb994c7c31d456c25a14e6a2
Benjamin-tech02 <yamena@hiof.no>
2024-11-21 13:29:26 +0100

    .
    .

--- prototype/src/Main.java
+++ prototype/src/Main.java
@@ -3,16 +3,6 @@ import org.SHA.core.usecase.ControlDeviceUseCase;
 
 public class Main {
     public static void main(String[] args) {
-        // Opprett en instans av ControlDeviceUseCase
-        ControlDeviceUseCase controlDeviceUseCase = new ControlDeviceUseCase();
 
-        // Demonstrer hvordan du sender en kommando til enheten
-        String deviceId = "device123";
-        String command = "turn on";
-        controlDeviceUseCase.executeCommand(deviceId, command);
-
-        // Demonstrer hvordan du henter statusen til en enhet
-        String status = controlDeviceUseCase.retrieveDeviceStatus(deviceId);
-        System.out.println(status);
     }
 }
